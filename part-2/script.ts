7b251db933cf690ed3bb7d86ae1494441d340de5
ericstaahl <94179820+ericstaahl@users.noreply.github.com>
2023-04-29 12:13:06 +0200

    esmodule

--- part-2/script.ts
+++ part-2/script.ts
@@ -1,11 +1,13 @@
-const fs = require('fs');
-const path = require('path')
+import fs from "fs";
+import path from "path";
 
-const file = fs.readFileSync(path.resolve(__dirname, "./series02.txt"), { encoding: 'utf-8' });
+const file = fs.readFileSync(path.resolve(__dirname, "./series02.txt"), {
+    encoding: "utf-8",
+});
 
 const players = file.split(/\r?\n/);
 
-const playerScores = players.map(player => {
+const playerScores = players.map((player) => {
     // get name of player
     const name = player.split(/[0-9]/)[0];
     // get scores of player
@@ -14,9 +16,9 @@ const playerScores = players.map(player => {
     const frames = [];
 
     // get array of all individual scores
-    const arr = scores.split(' ').map((score) => Number(score))
+    const arr = scores.split(" ").map((score) => Number(score));
 
-    let lastIndex = null
+    let lastIndex = null;
 
     for (let index = 0; index < arr.length; index++) {
         const score = arr[index];
@@ -26,9 +28,9 @@ const playerScores = players.map(player => {
             lastIndex = index;
             frames.push([10]);
             continue;
-        };
+        }
         if (lastIndex !== index && nextScore) {
-            lastIndex = (index + 1);
+            lastIndex = index + 1;
             frames.push([score, nextScore]);
             continue;
         }
@@ -37,29 +39,30 @@ const playerScores = players.map(player => {
 
     let totalScore = 0;
 
-    frames.forEach(
-        (frame) => {
-            if (frame[0] === 10) {
-                console.log("frame0:", frame[0], 'frame1:', frame[1])
-                totalScore += 10;
-                totalScore += frame[0];
-                return;
-            }
-
-            if (frame[0] + frame[1] === 10) {
-                totalScore += 5;
-                totalScore += frame[0];
-                totalScore += frame[1];
-            }
+    frames.forEach((frame) => {
+        if (frame[0] === 10) {
+            console.log("frame0:", frame[0], "frame1:", frame[1]);
+            totalScore += 10;
+            totalScore += frame[0];
+            return;
         }
-    );
+
+        if (frame[0] + frame[1] === 10) {
+            totalScore += 5;
+            totalScore += frame[0];
+            totalScore += frame[1];
+        }
+    });
     return { name, totalScore };
 });
 
-const compareFn = (a, b) => a.totalScore > b.totalScore ? -1 : 1
+const compareFn = (
+    a: { name: string; totalScore: number },
+    b: { name: string; totalScore: number }
+) => (a.totalScore > b.totalScore ? -1 : 1);
 
 // console.log(playerScores.sort((a, b) => compareFn(a, b)))
 
 const winner = playerScores.sort((a, b) => compareFn(a, b))[0];
 
-console.log(winner)
+console.log(winner);
